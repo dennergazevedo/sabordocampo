9ef7bd7433f384342b415706bd069dae36208b1e
unknown <dennergazevedo@gmail.com>
2020-08-10 12:16:25 -0300

    API HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://162.241.102.200:4257',
+    baseURL:'https://vps-3922624.artcopias.com.br:4257',
 });
 
 export default api;
